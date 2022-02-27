a2bf411f754c9746c04641ea8f1fe58debeaf390
Bitheral <me@bitheral.net>
2022-02-27 22:50:01 +0000

    Improved Demo level to show basic gameplay mechanics & fixed bugs in #26

--- Assets/scripts/Game/Activator.cs
+++ Assets/scripts/Game/Activator.cs
@@ -6,18 +6,23 @@ using UnityEngine.Events;
 public class Activator : MonoBehaviour
 {
 
+    [Header("How many executions allowed")]
+    public int executions = 1;
     public float executeAfterSeconds = 1.5f;
 
     [Header("Events to execute"), Tooltip("Add events here when object enters collider")]
     public UnityEvent actionEvents;
 
+    private int executionCount = 0;
+
     IEnumerator Execute(float seconds, UnityEvent action)
     {
         yield return new WaitForSeconds(seconds);
         action.Invoke();
+        executionCount++;
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Rigidbody grabbedRigidbody = GameManager.getPlayer().GetComponent<GrabItem>().grabbedItem;
         GameObject grabbedItem = grabbedRigidbody != null ? grabbedRigidbody.gameObject : null;
@@ -27,9 +32,17 @@ public class Activator : MonoBehaviour
             // Peform activation
             if(other.gameObject != grabbedItem)
             {
-                StartCoroutine(Execute(executeAfterSeconds, actionEvents));
+                if(executionCount < executions) StartCoroutine(Execute(executeAfterSeconds, actionEvents));
             }
 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other != null && other.gameObject.tag == "ScalableObject")
+        {
+            executionCount--;
         }
     }
 }
