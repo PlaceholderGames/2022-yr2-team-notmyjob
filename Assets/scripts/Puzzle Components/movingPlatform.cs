73707622c31788e2c8d04a75581e09268ed8fa1f
Bitheral <me@bitheral.net>
2022-05-05 00:33:23 +0100

    Fixed up hierarchy for Level 3 when player is on moving platform
    Fixed hierarchy on moving platform to prevent player from scaling when player jumps onto the platform, rotating, then jumps off, setting their scale off-nominal.

--- Assets/scripts/Puzzle Components/movingPlatform.cs
+++ Assets/scripts/Puzzle Components/movingPlatform.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class movingPlatform : MonoBehaviour
 {
-    //this should be the player, using it so the player doesnt' fall off the platform
-    [SerializeField] CharacterController Player;
 
     [SerializeField] GameObject Player_Pos;
     //when the player enters the collusion box it will have its position updated with the moving platform
     private void OnTriggerEnter(Collider other)
     {
-        print(other);
-        if (other == Player)
+        if (other.transform == Player_Pos.transform)
         {
             Player_Pos.transform.parent = gameObject.transform;
         }
@@ -21,7 +18,7 @@ public class movingPlatform : MonoBehaviour
     //when the player exits(jumps off) there will be no other factor affecting the player position outside of the player input
     private void OnTriggerExit(Collider other)
     {
-        if (other == Player)
+        if (other.transform == Player_Pos.transform)
         {
             Player_Pos.transform.parent = null;
         }
